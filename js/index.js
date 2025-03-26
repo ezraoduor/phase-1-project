218299da0d4009cf77437a74839f32777ed43bff
Ezra Oduor <ezrachurchill20@gmail.com>
2025-03-26 20:56:39 +0300

    adding db json

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
-function fetchMovies() {
-    fetch("http://localhost:3000/favorites")
-        .then(response => response.json())
-        .then(data => displayMovies(data))
-        .catch(error => console.error("Error fetching movies:", error));
-}
+
+
+
+
+
