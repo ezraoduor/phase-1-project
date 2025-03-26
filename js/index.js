fe9e8a33b1cbe59a8f63d71a30c0f68434070b60
Ezra Oduor <ezrachurchill20@gmail.com>
2025-03-26 21:22:19 +0300

    adding a display movies function

--- js/index.js
+++ js/index.js
@@ -1,4 +1,28 @@
+function fetchMovies() {
+    fetch("http://localhost:3000/favorites")
+    .then(response => response.json())
+    .then(data => displayMovies(data))
+    .catch(error => console.error("Error fetching movies:", error));
+}
 
+function displayMovies(movies) {
+    let movieList = document.getElementById("movie-list");
+    movieList.innerHTML = "";
+
+    movies.forEach(movie => {
+    let movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+
+    movieCard.innerHTML = `
+    <img src="${movie.poster}" alt="${movie.title}">
+    <h3>${movie.title}</h3>
+    <p>${movie.description}</p>
+    <button onclick="addToFavorites('${movie.id}', '${movie.title}', '${movie.poster}')">Add to Favorites</button>
+`;
+
+    movieList.appendChild(movieCard);
+});
+}
 
 
 
